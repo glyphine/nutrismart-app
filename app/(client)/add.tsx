abd12133f78d68d972e81255c04ecc51d66d0eb8
Gwynne <lobsgwynne37@gmail.com>
2025-05-29 01:53:43 +0800

    labelled pages

--- app/(client)/add.tsx
+++ app/(client)/add.tsx
@@ -1,9 +1,9 @@
-import { View, Text } from "react-native";
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-600">Hello, world!</Text>
+      <Text className="text-xl font-bold text-blue-600">Scan</Text>
     </View>
   );
 }
--- app/(client)/inventory.tsx
+++ app/(client)/inventory.tsx
@@ -3,7 +3,7 @@ import { View, Text } from "react-native";
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-600">Hello, world!</Text>
+      <Text className="text-xl font-bold text-blue-600">Inventory</Text>
     </View>
   );
 }
