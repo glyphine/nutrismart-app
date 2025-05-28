813bc805439f39eba11e631a90fc5308b6883ce4
Gwynne <lobsgwynne37@gmail.com>
2025-05-29 05:21:51 +0800

    added _layout on recipes

---
+++ app/(client)/recipes/_layout.tsx
@@ -0,0 +1,15 @@
+import React, { useRef } from "react";
+import { Stack, Tabs } from "expo-router";
+import { View, Text } from "react-native";
+
+
+export default function RecipeLayout() {
+
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+      }}
+    />
+  );
+}
